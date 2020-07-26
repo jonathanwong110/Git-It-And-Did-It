57786f23c6837f3868303165f118a8b887f12ecf
jonathanwong110 <jonathan.wong110@gmail.com>
2020-07-26 16:29:03 -0400

    created the user controller

---
+++ git-it-and-did-it-api/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only [:show, :update, :destroy]
+  
+  def index
+    @users = User.all
+    render json: @users, status: 200
+  end
+
+  def show
+    render json: @user, status: 200
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: 200
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, status: 200
+    end
+  end
+
+  def destroy
+    @user.destroy
+    render json: {userId: @user.id}
+  end
+
+  private
+    def set_user
+      @user = user.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :username, :password)
+    end
+
+end
