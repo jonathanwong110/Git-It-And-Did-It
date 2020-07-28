ba2945c4ee47447870000cac86d5465a9c62c674
jonathanwong110 <jonathan.wong110@gmail.com>
2020-07-27 22:35:13 -0400

    updated user_tasks controller

--- git-it-and-did-it-api/app/controllers/api/v1/user_tasks_controller.rb
+++ git-it-and-did-it-api/app/controllers/api/v1/user_tasks_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::UserTasksController < ApplicationController
+
+  def create
+    @user_task = @user.where(user_id: current_user.id)
+    render @task
+  end
+
+  def destroy
+    @user_task = @user.where(user_id: current_user.id)
+    @user_task.destroy
+    render json: {message: 'You have deleted the task'}
+  end
+
 end
