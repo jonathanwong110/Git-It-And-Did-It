f20cbecdf3db418aa19cd2634ececcc4a71af53a
jonathanwong110 <jonathan.wong110@gmail.com>
2020-07-27 22:43:18 -0400

    updated user and task models

--- git-it-and-did-it-api/app/models/task.rb
+++ git-it-and-did-it-api/app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  has_many :user_tasks
+  has_many :users, through: user_tasks
+  validates_presence_of :title, category, description, status, priority, :user
 end
--- git-it-and-did-it-api/app/models/user.rb
+++ git-it-and-did-it-api/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :user_tasks
+  has_many :tasks, through :user_tasks
+  validates_presence_of :email, :username, :password
 end
