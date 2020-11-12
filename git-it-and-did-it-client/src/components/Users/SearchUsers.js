24db9703d027e49141c827327e643cbbcb8435f7
jonathanwong110 <jonathan.wong110@gmail.com>
2020-11-11 23:25:07 -0500

    positioned the search bar better

--- git-it-and-did-it-client/src/components/Users/SearchUsers.js
+++ git-it-and-did-it-client/src/components/Users/SearchUsers.js
@@ -8,7 +8,7 @@ export default function SearchUsers(props) {
   return (
     <div>
       <input type="text" name="search" className="searchForm" value={props.searchEntry} placeholder="Search" onKeyPress={props.onKeyPress} onChange={e => props.handleChange(e)} />
-      <Button variant="primary" id="searchUsers" type="submit" onClick={e => props.handleSubmit(e)}>
+      <Button variant="primary" className="searchButton" type="submit" onClick={e => props.handleSubmit(e)}>
         <FontAwesomeIcon icon={faSearch}/>
       </Button>
     </div>
